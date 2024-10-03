0693818e1af8e3eca88d336c8d4389be280a2b61
Srilatha <koduri0311@gmail.com>
2024-10-03 14:03:39 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+
+}
+
+variable "environment" {
+    
+}
+
+variable "sg_name" {
+    
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "sg_tags" {
+    default = {}
+}
+
+variable "vpc_id" {
+   
+}
